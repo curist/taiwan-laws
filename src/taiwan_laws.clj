a7ef6c079a240033aefb55f96c2e4f12c523bf0b
curist <curist.cyc@gmail.com>
2025-03-11 10:29:57 +0800

    organize articles

--- src/taiwan_laws.clj
+++ src/taiwan_laws.clj
@@ -4,7 +4,8 @@
             [babashka.fs :as fs]
             [babashka.http-client :as http]
             [babashka.process :refer [shell]]
-            [cheshire.core :as json]))
+            [cheshire.core :as json]
+            [selmer.parser :refer [render-file]]))
 
 (def ch-laws-url "https://law.moj.gov.tw/api/data/chlaw.json.zip")
 (def ch-orders-url "https://law.moj.gov.tw/api/data/chorder.json.zip")
@@ -30,18 +31,91 @@
                ["blob/ChOrder.json" "blob/order.jq.json"]]]
     (run! jq-process-json jsons)))
 
-(defn extract-laws [laws base-dir]
-  (-> (fn [law]
-        (let [dir (->> law :LawCategory
-                       (re-seq #"[^＞\s]+")
-                       (string/join "/")
-                       (str base-dir "/"))
-              law-name (:LawName law)
-              law-name (string/replace law-name #"（.+）$" "")
-              file-path (str dir "/" law-name ".json")]
-          (fs/create-dirs dir)
-          (spit file-path (json/encode law {:pretty true}))))
-      (run! laws)))
+(defn- format-modified-date [s]
+  (.format
+   (java.text.SimpleDateFormat. "yyyy/MM/dd")
+   (.parse
+    (java.text.SimpleDateFormat. "yyyyMMdd")
+    s)))
+
+(defn- trim-line-breaks [s]
+  (string/replace s #"\r\n" ""))
+
+(defn- is-section? [s]
+  (re-find #"\s.+\s節\s.+" s))
+
+(defn- compose-law-articles [articles]
+  (->> articles
+       (map (fn [{:keys [ArticleType ArticleNo ArticleContent]}]
+              (if (= "C" ArticleType)
+                (if (is-section? ArticleContent)
+                  (str "#### " (string/trim ArticleContent))
+                  (str "### " (string/trim ArticleContent)))
+                ;; articles
+                (str "##### " ArticleNo "\n" ArticleContent ;; TODO: formatting this
+                     ))))
+       (string/join "\n\n")))
+
+(defn- law-json->markdown [law]
+  (let [law-name (:LawName law)
+        law-name-note (re-find #"（.+）$" law-name)
+        law-name (string/replace law-name #"（.+）$" "")
+        foreword (:LawForeword law)
+        foreword (if (empty? foreword) "" (trim-line-breaks foreword))
+        articles (compose-law-articles (:LawArticles law))]
+    (render-file
+     "law.md.template"
+     {:name law-name
+      :name-note law-name-note
+      :category (:LawCategory law)
+      :modified-date (format-modified-date (:LawModifiedDate law))
+      :url (:LawURL law)
+      :foreword foreword
+      :abandoned (:LawAbandonNote law)
+      ;; :chapters :TODO
+      :articles articles
+      ;; :attachments :TODO
+      ;; :histories :TODO
+      })))
+
+(defn- process-law-json [law base-dir]
+  (let [dir (->> law :LawCategory
+                 (re-seq #"[^＞\s]+")
+                 (string/join "/")
+                 (str base-dir "/"))
+        law-name (:LawName law)
+        law-name (string/replace law-name #"（.+）$" "")
+        json-path (str dir "/" law-name ".json")
+        md-path (str dir "/" law-name ".md")]
+    (fs/create-dirs dir)
+    (spit json-path (json/encode law {:pretty true}))
+    (spit md-path (law-json->markdown law))))
+
+(defn process-law-jsons [laws base-dir]
+  (run! #(process-law-json % base-dir) laws))
+
+; (def the-law :the-law) ;; (first laws)
+; (->> (:LawArticles the-law)
+;      (partition-by :ArticleType)
+;      (partition 2))
+
+;; check article 27 & https://law.moj.gov.tw/LawClass/LawSingle.aspx?pcode=A0000001&flno=27
+;; 30 also
+;; should handle 節, like https://law.moj.gov.tw/LawClass/LawAllPara.aspx?pcode=A0000001
+;; TODO: can we use chinese chapter names & use them as github markdown TOC target?
+;; # LawName
+;; ## laws 法條
+;; ### 章 chapter
+;; #### 節 chapter
+;; ##### 條 article
+;;
+;; interesting law fields
+;; LawModifiedDate 法規異動日期
+;; LawLevel 法規位階
+;; LawForeword 前言
+;; LawHistories
+;; LawURL
+;; LawAttachements?
 
 (comment
   (def laws (-> (io/reader "blob/law.jq.json") (json/parse-stream true) :Laws))
